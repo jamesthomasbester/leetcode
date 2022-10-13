6b4f72130b06c299d21721fe95910e8ac3767c51
James Bester <46641259+jamesthomasbester@users.noreply.github.com>
2022-10-13 13:46:53 +1100

    Add files via upload

--- validPar.js
+++ validPar.js
@@ -1,10 +1,28 @@
 var isValid = function(s) {
-    if(s.match(/((\(\)|\{\}|\[\]|\{(\[\])\}|\{(\(\))\}|\{(\{\})\}))/g)){
+    let comparitor = ['[','{','('];
+    let bracketMap = new Map();
+    bracketMap.set(']', '[');
+    bracketMap.set('}', '{');
+    bracketMap.set(')', '(');
+    let stack = []
+    for (let i = 0; i < s.length; i++){
+        if (comparitor.includes(s[i])){
+            stack.push(s[i])
+        }else if(stack[stack.length-1] == bracketMap.get(s[i])){
+            stack.pop();
+            console.log(i)
+        }else{
+            stack.push(s[i])
+        }
+        console.log(s[i])
+        console.log(bracketMap.get(s[i]))
+    }
+    console.log(stack)
+    if (stack.length == 0){
         return true;
-    } 
-    else{
+    }else{
         return false;
     }
 };
 
-console.log(isValid('{ }[ ]'))
+console.log(isValid("()[]"))
