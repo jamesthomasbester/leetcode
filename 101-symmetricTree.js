5b8bcf656b9843bd09f8513a832f627b06eba0eb
James Bester <46641259+jamesthomasbester@users.noreply.github.com>
2022-11-09 14:39:58 +1100

    Add files via upload

--- 101-symmetricTree.js
+++ 101-symmetricTree.js
@@ -1,3 +1,4 @@
+// 101. Symmetric Tree - https://leetcode.com/problems/symmetric-tree/
 /**
  * Definition for a binary tree node.
  * function TreeNode(val, left, right) {
@@ -10,6 +11,22 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
+
  var isSymmetric = function(root) {
-    
+    var memory = []
+    var count = 0;
+    var test = function(val) {
+        console.log(val)
+        if(!val){
+            count ++;
+            console.log(memory)
+            console.log(count)
+            return true
+        }else{
+            memory.push(val.val)
+            return test(val.left) && test(val.right)
+        }
+    }
+    test(root)
+
 };
