3053326134b3f5fbee1856c7a3f3be41a369af80
James Bester <jamesthomasbester@gmail.com>
2023-07-04 17:02:08 +1000

    testing

--- 2-AddTwoNumbers.js
+++ 2-AddTwoNumbers.js
@@ -1,4 +1,3 @@
-// 2. Add Two Numbers - https://leetcode.com/problems/add-two-numbers/
 /**
  * Definition for singly-linked list.
  * function ListNode(val, next) {
@@ -11,6 +10,32 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
- var addTwoNumbers = function(l1, l2) {
-    
+var addTwoNumbers = function(l1, l2) {
+    let lastNode = new ListNode()
+    let masterNode = lastNode
+    let remander;
+    while(l1 || l2){
+        let val1 = 0
+        let val2 = 0
+        if(l1){
+            val1 = l1.val
+            l1 = l1.next
+        }
+        if(l2){
+            val2 = l2.val
+            l2 = l2.next
+        }
+        if(remander){
+            sum += 1
+        }
+        let sum = val1 + val2
+        if(sum > 9){
+            remander = 1
+
+        }
+        const node = new ListNode(sum)
+        lastNode.next = node
+        lastNode = node
+    }
+    return masterNode
 };
